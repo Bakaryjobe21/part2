2d31b532a296bc18f0ecd05ee5702067a56baf6c
Bakaryjobe21 <Bakaryjobe21@gmail.com>
2021-10-09 16:44:54 +0000

    2.6: The Phonebook Step1

--- part-2/src/App.js
+++ part-2/src/App.js
@@ -1,67 +1,59 @@
-import  {React, useState} from "react"
-import Course from "./components/Course"
+import React, { useState } from 'react'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const name=(e)=>{
+    setNewName(e.target.value)
+  }
+
+  const  addPerson= (event) => {
+    event.preventDefault()
+    //const {secName} =newName;
+    const noteObject = {
+
+      name: newName,
+      id:1
     }
-  ]
+   // const isPerson =persons.some(p=>{p.secName===name})?
+    //noteObject.name===secName?  window.alert(`${noteObject.name} is not part of the phonebook`):
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+  }
+  
+ // ? notes
+  //: notes.filter(note => note.important === true)
 
 
   
 
-  return (<div><h1>Web Development Curriculum</h1>
-
-    <Course course={courses} />
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={name}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((names)=>{
+        return <div> {names.name}</div>
+      })}
       
-      </div>
-     
-    )
-  }
+    </div>
+          
+    
+    
+  )
+}
 
 export default App
-
-
 
 
 
