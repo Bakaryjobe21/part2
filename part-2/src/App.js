3c76bdd9c9abad7c8f907a9644b533f2724f0116
Bakaryjobe21 <Bakaryjobe21@gmail.com>
2021-10-21 15:01:28 +0000

    2.15: Phonebook step7

--- part-2/src/App.js
+++ part-2/src/App.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
+
+//import PersonForm from './components/PersonForm'
+//import Persons from './components/Persons'
 import Filter from './components/Filter'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',number:1829}
-  ]) 
+
+  const [persons, setPersons] = useState([])
+  //const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
+  // ...
+
+ // const [ persons, setPersons ] = useState([
+ //   { name: 'Arto Hellas',number:1829}
+ // ]) 
   const [ newName, setNewName ] = useState('')
-  const[search,setSearch]=useState('')
-  const [newNumber,setNewNumber]=useState(0)
+ // const[search,setSearch]=useState('')
+  const [newNumber,setNewNumber]=useState()
 
   const name=(e)=>{
     setNewName(e.target.value)
   }
-  const name2=(e)=>{
-    setSearch(e.target.value)
-  }
+ // const name2=(e)=>{
+  //  setSearch(e.target.value)
+  //}
 
   const number=(e)=>{
     setNewNumber(e.target.value)
@@ -29,11 +49,18 @@ const App = () => {
     const noteObject = {
 
       name: newName,
-      number:newNumber
+      number:newNumber,
+      
     }
+
+    axios
+    .post('http://localhost:3001/persons', noteObject)
+    .then(response => {
+      setPersons(persons.concat(response.data))
+    })
     
     //noteObject.name===secName?  window.alert(`${noteObject.name} is not part of the phonebook`):
-    setPersons(persons.concat(noteObject))
+    
     const isPerson =persons.some(p=>p.name===newName)
 
   if(isPerson){
@@ -41,6 +68,8 @@ const App = () => {
 
 
   }
+
+  
    
     setNewName('')
     setNewNumber()
@@ -59,14 +88,27 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
 
-       <Filter persons={persons} newName={newName} name2={name2} addPerson={addPerson}/>
 
+
+       
       <h3>Add a new</h3>
-      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} name={name} number={number}/>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={name}/>
+        </div>
+         <div>number: <input value={newNumber} onChange={number} /></div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+        </form>
       
       <h3>Numbers</h3>
+      {persons.map((names,index)=>{
+        return <div> <p key={index}>{names.name} {names.number}</p></div>
+      })}
 
-      <Persons persons={persons}/>
+    
      
       
     </div>
@@ -76,7 +118,14 @@ const App = () => {
   )
 }
 
+
+
+
+
 export default App
 
 
+//<PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} name={name} number={number}/>
+// <Persons persons={persons}/>
 
+//<Filter persons={persons} newName={newName} name2={name2} addPerson={addPerson}/>
